fd8ab7eda0ba67390da5169f59887f04700b0aa6
Mukesh <mukesh51@gmail.com>
2016-09-23 15:34:26 +0100

    Added navMenuObjects

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,17 +1,23 @@
 import {Injectable} from '@angular/core';
-import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable, AuthProviders, AuthMethods } from 'angularfire2';
 
 @Injectable()
 export class UserService {
 	users: FirebaseListObservable<any>;
 	navMenuItems: FirebaseListObservable<any>;
+	navMenuItemsasObjects: FirebaseObjectObservable<any>;
 
 	constructor(_af: AngularFire) {		
 		this.users = _af.database.list('https://flickering-heat-8853.firebaseio.com/users');
-			
 		
+			
 		// this.navMenuItems = af.database.list('https://flickering-heat-8853.firebaseio.com/navMenu');
 		// console.log("this.navMenuItems "+this.navMenuItems.subscribe(snapshot => console.log(snapshot)));
+		
+		//lift, remove, set, update - FirebaseObjectObservable
+		//lift, push, remove, update - FirebaseListObservable
+		//createUser, getAuth, login, logout - AngularFireAuth
+		
 		
 	}
 
